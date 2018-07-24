caf61db2c19bf44ca4f6eac6ea5c1b6c82908246
hokieknight <dale.mrotek@gmail.com>
2018-07-23 21:00:53 -0400

    Add files via upload

---
+++ magic_number.py
@@ -0,0 +1,15 @@
+
+def gen_magic_number():
+	return 4
+
+def get_user_number():
+	return int(input("Guess a number between 0 and 10: "))
+
+def magic_number():
+
+	if (get_user_number() == gen_magic_number()):
+		print("Correct!")
+	else:
+		print("Try Again")
+
+magic_number()
